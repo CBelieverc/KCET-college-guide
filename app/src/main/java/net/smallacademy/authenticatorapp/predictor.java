77cb9d5629cb228156394b7a4f5b9c0cadfc402c
cchan <cchandanm52@gmail.com>
2022-09-19 14:59:07 +0530

    debugged

--- app/src/main/java/net/smallacademy/authenticatorapp/predictor.java
+++ app/src/main/java/net/smallacademy/authenticatorapp/predictor.java
@@ -1,42 +1,57 @@
 package net.smallacademy.authenticatorapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class predictor extends AppCompatActivity {
-    final String URL = "https://www.inspirenignite.com/kcet-cutoff-closing-ranks-and-college-predictor/";
-    @SuppressLint("setJavaScriptEnabled")
+
+    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predictor);
-        ImageView back6 =findViewById(R.id.back6);
-        back6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        final WebView mWebView = (WebView) findViewById(R.id.web1);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setSupportZoom(true);
-        mWebView.getSettings().setBuiltInZoomControls(true);
-        mWebView.getSettings().setDisplayZoomControls(false);
-        mWebView.setWebViewClient(new WebViewClient(){
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                mWebView.loadUrl("javascript:(function() { " +
-                        "var head = document.getElementsByClassName('s-header')[0].style.display='none'; " +
-                        "})()");
-            }
-        });
-        mWebView.loadUrl(URL);
+        AdBlocker.init(this);
 
+        webview=(WebView)findViewById(R.id.web1);
+        webview.setWebViewClient(new MyBrowser());
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webview.loadUrl("https://www.inspirenignite.com/kcet-cutoff-closing-ranks-and-college-predictor/");
+
+    }
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        private Map<String, Boolean> loadedUrls = new HashMap<>();
+        @Nullable
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+            boolean ad;
+            if (!loadedUrls.containsKey(url)) {
+                ad = AdBlocker.isAd(url);
+                loadedUrls.put(url, ad);
+            } else {
+                ad = loadedUrls.get(url);
+            }
+            return ad ? AdBlocker.createEmptyResource() :
+                    super.shouldInterceptRequest(view, url);
+        }
     }
 }
